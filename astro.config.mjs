8059dd5ee43888b0a001c198cf8f224c5276f088
sebastianFeltes <jikansui16@gmail.com>
2026-05-05 01:26:08 -0300

    fixing routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sebastianFeltes.github.io",
-  base: "/exfutura-portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
